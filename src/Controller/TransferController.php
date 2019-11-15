7dab7eb2d8237c262748feae8c9b65ed27db21f4
Jonathan XERRI <jxerri145@gmail.com>
2019-11-15 17:30:03 +0100

    Ajout clé privé et public et modif virement

--- src/Controller/TransferController.php
+++ src/Controller/TransferController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TransferController extends AbstractController
@@ -10,19 +12,22 @@ class TransferController extends AbstractController
     /**
      * @Route("/transfer", name="transfer")
      */
-    public function index()
+    public function intern(Request $request)
     {
         $data = json_decode($request->getContent(), true);
         $Emetteur = $data['compteEmetteur'];
         $Recepteur = $data['compteRecepteur'];
-        $Solde = $data['solde'];
+        $Solde = $data['sold'];
         var_dump($Emetteur);
         var_dump($Recepteur);
         var_dump($Solde);
 
         $em = $this->getDoctrine()->getManager();
-        $product = $em->getRepository(User::class)->findOneBy(array('numberAccount' => $Recepteur));
-        $product->setSold(200 + $Solde);
+        $emetteur = $em->getRepository(User::class)->findOneBy(array('numberAccount' => $Emetteur));
+        $emetteur->setSold($emetteur->getSold() - $Solde);
+        $recepteur = $em->getRepository(User::class)->findOneBy(array('numberAccount' => $Recepteur));
+        $recepteur->setSold($recepteur->getSold() + $Solde);
         $em->flush();
+
     }
 }
